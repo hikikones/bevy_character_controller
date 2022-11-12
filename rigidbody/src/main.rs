805f71d567ff40f2a1f21acae40470f85035b33b
hikikones <hikikones@gmail.com>
2022-11-12 11:01:06 +0100

    simulation

--- rigidbody/src/main.rs
+++ rigidbody/src/main.rs
@@ -1,8 +1,11 @@
-use bevy::{ecs::schedule::ShouldRun, prelude::*};
-use bevy_rapier3d::prelude::*;
+use bevy::prelude::*;
 
 use bevy_extensions::*;
 use bootstrap::*;
+
+mod simulation;
+
+use simulation::*;
 
 fn main() {
     App::new()
@@ -11,174 +14,24 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(BootstrapPlugin)
-        .add_plugin(PhysicsPlugin)
+        .add_plugin(SimulationPlugin)
         .add_startup_system(setup)
         .add_system_set_to_stage(
             CoreStage::Update,
             SystemSet::new()
-                .with_system(movement)
                 .with_system(rotation)
-                .with_system(jump),
+                .with_system(jump)
+                .with_system(lerp),
+        )
+        .add_system_set_to_stage(
+            SimulationStage::Update,
+            SystemSet::new().with_system(movement),
+        )
+        .add_system_set_to_stage(
+            SimulationStage::PostUpdate,
+            SystemSet::new().with_system(lerp_set),
         )
         .run();
-}
-
-const SYNC_STAGE: &str = "sync_stage";
-
-struct PhysicsPlugin;
-
-impl Plugin for PhysicsPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(RapierConfiguration {
-            timestep_mode: TimestepMode::Interpolated {
-                dt: TICK,
-                time_scale: 1.0,
-                substeps: 1,
-            },
-            // force_update_from_transform_changes: false,
-            ..Default::default()
-        })
-        .insert_resource(FixedTime::default())
-        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default().with_default_system_setup(true))
-        .add_plugin(RapierDebugRenderPlugin::default())
-        // .add_system_to_stage(CoreStage::First, print_dt)
-        .add_stage_after(
-            PhysicsStages::Writeback,
-            SYNC_STAGE,
-            SystemStage::parallel()
-                .with_run_criteria(fixed_run_criteria)
-                // .with_system(sync_test),
-                .with_system(set_lerp),
-        )
-        .add_system(lerp)
-        // .add_stage_after(
-        //     PhysicsStages::Writeback,
-        //     SYNC_STAGE,
-        //     SystemStage::single(sync_test).with_run_criteria(rapier_run_criteria),
-        // )
-        // .add_system_to_stage(CoreStage::PostUpdate, sync_test)
-        .add_system_to_stage(CoreStage::Last, fixed_time);
-
-        // app.add_stage_after(
-        //     CoreStage::Update,
-        //     PhysicsStages::SyncBackend,
-        //     SystemStage::parallel()
-        //         // .with_run_criteria(fixed_run_criteria)
-        //         .with_system_set(RapierPhysicsPlugin::<NoUserData>::get_systems(
-        //             PhysicsStages::SyncBackend,
-        //         )),
-        // );
-
-        // app.add_stage_after(
-        //     PhysicsStages::SyncBackend,
-        //     PhysicsStages::StepSimulation,
-        //     SystemStage::parallel()
-        //         .with_run_criteria(fixed_run_criteria)
-        //         .with_system_set(RapierPhysicsPlugin::<NoUserData>::get_systems(
-        //             PhysicsStages::StepSimulation,
-        //         )),
-        // );
-
-        // app.add_stage_after(
-        //     PhysicsStages::StepSimulation,
-        //     PhysicsStages::Writeback,
-        //     SystemStage::parallel()
-        //         .with_run_criteria(fixed_run_criteria)
-        //         // .with_system_set(RapierPhysicsPlugin::<NoUserData>::get_systems(
-        //         //     PhysicsStages::Writeback,
-        //         // )),
-        //         .with_system(sync),
-        // );
-
-        // // app.add_stage_after(
-        // //     PhysicsStages::StepSimulation,
-        // //     PhysicsStages::Writeback,
-        // //     SystemStage::parallel()
-        // //         // .with_run_criteria(fixed_run_criteria)
-        // //         .with_system_set(RapierPhysicsPlugin::<NoUserData>::get_systems(
-        // //             PhysicsStages::Writeback,
-        // //         )),
-        // // );
-
-        // // NOTE: we run sync_removals at the end of the frame, too, in order to make sure we don’t miss any `RemovedComponents`.
-        // app.add_stage_before(
-        //     CoreStage::Last,
-        //     PhysicsStages::DetectDespawn,
-        //     SystemStage::parallel()
-        //         // .with_run_criteria(fixed_run_criteria)
-        //         .with_system_set(RapierPhysicsPlugin::<NoUserData>::get_systems(
-        //             PhysicsStages::DetectDespawn,
-        //         )),
-        // );
-    }
-}
-
-const TICK: f32 = 1.0 / 5.0;
-
-#[derive(Default)]
-struct FixedTime(f32);
-
-impl FixedTime {
-    fn percent(&self) -> f32 {
-        self.0 / TICK
-    }
-}
-
-fn print_dt(sim: Res<SimulationToRenderTime>) {
-    dbg!(sim.diff);
-}
-
-fn rapier_run_criteria(sim: Res<SimulationToRenderTime>, ctx: Res<RapierContext>) -> ShouldRun {
-    dbg!(sim.diff);
-    match sim.diff >= ctx.integration_parameters.dt {
-        true => ShouldRun::Yes,
-        false => ShouldRun::No,
-    }
-}
-
-fn sync_test(
-    mut actor_q: Query<&mut Transform, (With<Actor>, Without<Player>)>,
-    player_q: Query<&Transform, With<Player>>,
-) {
-    actor_q.single_mut().translation = player_q.single().translation;
-}
-
-#[derive(Default, Component)]
-struct Lerp(Vec3, Vec3);
-
-fn set_lerp(
-    mut actor_q: Query<&mut Lerp, (With<Actor>, Without<Player>)>,
-    player_q: Query<&Transform, With<Player>>,
-) {
-    let mut lerp = actor_q.single_mut();
-    lerp.0 = lerp.1;
-    lerp.1 = player_q.single().translation;
-}
-
-fn lerp(mut actor_q: Query<(&mut Transform, &Lerp), With<Actor>>, fixed_time: Res<FixedTime>) {
-    let (mut transform, lerp) = actor_q.single_mut();
-    transform.translation = Vec3::lerp(lerp.0, lerp.1, fixed_time.percent());
-}
-
-// fn sync_test(player_q: Query<&Transform, With<Player>>) {
-//     for t in player_q.iter() {
-//         dbg!(t.translation);
-//     }
-// }
-
-fn fixed_run_criteria(fixed_time: Res<FixedTime>) -> ShouldRun {
-    match fixed_time.0 >= TICK {
-        true => ShouldRun::Yes,
-        false => ShouldRun::No,
-    }
-}
-
-fn fixed_time(mut fixed_time: ResMut<FixedTime>, time: Res<Time>) {
-    if fixed_time.0 >= TICK {
-        fixed_time.0 -= TICK;
-    }
-
-    fixed_time.0 += time.delta_seconds();
 }
 
 #[derive(Component)]
@@ -226,14 +79,6 @@ fn setup(platform_q: Query<(Entity, &Platform)>, mut commands: Commands) {
     }
 }
 
-fn sync(mut q: Query<(Entity, &mut Transform), With<Player>>, ctx: Res<RapierContext>) {
-    let (entity, mut transform) = q.single_mut();
-    let handle = ctx.entity2body().get(&entity).unwrap();
-    let rb = ctx.bodies.get(*handle).unwrap();
-    let pos = rb.position();
-    transform.translation = pos.translation.into();
-}
-
 const MAX_SPEED: f32 = 10.0;
 const MAX_ACCELERATION: f32 = MAX_SPEED * 2.0;
 const ROTATION_SPEED: f32 = MAX_SPEED * 1.5;
@@ -242,21 +87,18 @@ const JUMP_HEIGHT: f32 = 2.0;
 fn movement(
     mut player_q: Query<&mut Velocity, With<Player>>,
     input: Res<InputMovement>,
-    time: Res<Time>,
+    tick: Res<SimulationTick>,
 ) {
     let mut velocity = player_q.single_mut();
 
     let input = input.x0z();
-    let dt = time.delta_seconds();
     let target = input * MAX_SPEED;
-    let max_delta = MAX_ACCELERATION * TICK;
+    let max_delta = MAX_ACCELERATION * tick.rate();
 
     velocity.linvel = velocity
         .linvel
         .move_towards(target, max_delta)
         .x_z(velocity.linvel.y);
-
-    // dbg!(velocity.linvel);
 }
 
 fn rotation(
@@ -281,4 +123,21 @@ fn jump(mut player_q: Query<&mut ExternalImpulse, With<Player>>, input_action: R
         let force = Vec3::Y * f32::sqrt(2.0 * 9.81 * JUMP_HEIGHT);
         player_q.single_mut().impulse = force;
     }
+}
+
+#[derive(Default, Component)]
+struct Lerp(Vec3, Vec3);
+
+fn lerp(mut actor_q: Query<(&mut Transform, &Lerp), With<Actor>>, tick: Res<SimulationTick>) {
+    let (mut transform, lerp) = actor_q.single_mut();
+    transform.translation = Vec3::lerp(lerp.0, lerp.1, tick.percent());
+}
+
+fn lerp_set(
+    mut actor_q: Query<&mut Lerp, (With<Actor>, Without<Player>)>,
+    player_q: Query<&Transform, With<Player>>,
+) {
+    let mut lerp = actor_q.single_mut();
+    lerp.0 = lerp.1;
+    lerp.1 = player_q.single().translation;
 }
---
+++ rigidbody/src/simulation.rs
@@ -0,0 +1,78 @@
+use bevy::{ecs::schedule::ShouldRun, prelude::*};
+use bevy_rapier3d::prelude::*;
+
+pub use bevy_rapier3d::prelude::{
+    Ccd, Collider, CollisionGroups, Damping, ExternalForce, ExternalImpulse, Friction, LockedAxes,
+    RigidBody, Sleeping, Velocity,
+};
+
+pub struct SimulationPlugin;
+
+impl Plugin for SimulationPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(SimulationTick::default())
+            .insert_resource(RapierConfiguration {
+                timestep_mode: TimestepMode::Interpolated {
+                    dt: SIMULATION_TICK_RATE,
+                    time_scale: 1.0,
+                    substeps: 1,
+                },
+                ..Default::default()
+            })
+            .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+            .add_plugin(RapierDebugRenderPlugin::default())
+            .add_stage_before(
+                CoreStage::PreUpdate,
+                SimulationStage::PreUpdate,
+                SystemStage::parallel().with_run_criteria(run_criteria),
+            )
+            .add_stage_before(
+                CoreStage::Update,
+                SimulationStage::Update,
+                SystemStage::parallel().with_run_criteria(run_criteria),
+            )
+            .add_stage_after(
+                PhysicsStages::Writeback,
+                SimulationStage::PostUpdate,
+                SystemStage::parallel().with_run_criteria(run_criteria),
+            )
+            .add_system_to_stage(CoreStage::Last, tick);
+    }
+}
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
+pub enum SimulationStage {
+    PreUpdate,
+    Update,
+    PostUpdate,
+}
+
+#[derive(Default)]
+pub struct SimulationTick(f32);
+
+const SIMULATION_TICK_RATE: f32 = 1.0 / 5.0;
+
+impl SimulationTick {
+    pub const fn rate(&self) -> f32 {
+        SIMULATION_TICK_RATE
+    }
+
+    pub fn percent(&self) -> f32 {
+        self.0 / SIMULATION_TICK_RATE
+    }
+}
+
+fn tick(mut tick: ResMut<SimulationTick>, time: Res<Time>) {
+    if tick.0 >= SIMULATION_TICK_RATE {
+        tick.0 -= SIMULATION_TICK_RATE;
+    }
+
+    tick.0 += time.delta_seconds();
+}
+
+fn run_criteria(tick: Res<SimulationTick>) -> ShouldRun {
+    match tick.0 >= SIMULATION_TICK_RATE {
+        true => ShouldRun::Yes,
+        false => ShouldRun::No,
+    }
+}
