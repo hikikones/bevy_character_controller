a237f0361533d161369a52b28307e0a18db47f36
hikikones <hikikones@gmail.com>
2022-11-20 12:43:29 +0100

    looking good

--- manual/src/physics/mod.rs
+++ manual/src/physics/mod.rs
@@ -2,15 +2,33 @@ use bevy::prelude::*;
 
 use bevy_extensions::Vec3SwizzlesExt;
 
+mod tick;
+
+pub use tick::*;
+
+const PHYSICS_TICK_RATE: f32 = 1.0 / 10.0;
+
+#[derive(Debug, Hash, PartialEq, Eq, Clone, StageLabel)]
+pub enum PhysicsStage {
+    Update,
+    PostUpdate,
+}
+
 pub struct PhysicsPlugin;
 
 impl Plugin for PhysicsPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system_set_to_stage(
+        app.add_stage_before(
+            CoreStage::Update,
+            PhysicsStage::Update,
+            SystemStage::parallel().with_run_criteria(tick_run_criteria),
+        )
+        .add_stage_before(
             CoreStage::PostUpdate,
-            SystemSet::new()
-                .before(bevy::transform::transform_propagate_system)
-                .with_system(apply_velocity),
+            PhysicsStage::PostUpdate,
+            SystemStage::parallel()
+                .with_run_criteria(tick_run_criteria)
+                .with_system_set(SystemSet::new().with_system(apply_velocity)),
         );
     }
 }
---
+++ manual/src/physics/tick.rs
@@ -0,0 +1,40 @@
+use bevy::prelude::*;
+
+use super::*;
+
+pub(super) struct TickPlugin;
+
+impl Plugin for TickPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(PhysicsTick::default())
+            .add_system_to_stage(CoreStage::Last, advance_tick);
+    }
+}
+
+#[derive(Resource, Default)]
+pub struct PhysicsTick(f32);
+
+impl PhysicsTick {
+    pub const fn rate(&self) -> f32 {
+        PHYSICS_TICK_RATE
+    }
+
+    pub fn percent(&self) -> f32 {
+        self.0 / PHYSICS_TICK_RATE
+    }
+}
+
+fn advance_tick(mut tick: ResMut<PhysicsTick>, time: Res<Time>) {
+    if tick.0 >= PHYSICS_TICK_RATE {
+        tick.0 -= PHYSICS_TICK_RATE;
+    }
+
+    tick.0 += time.delta_seconds();
+}
+
+pub(super) fn tick_run_criteria(tick: Res<PhysicsTick>) -> ShouldRun {
+    match tick.0 >= PHYSICS_TICK_RATE {
+        true => ShouldRun::Yes,
+        false => ShouldRun::No,
+    }
+}
